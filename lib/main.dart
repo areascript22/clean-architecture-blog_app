e16e5ca0746fe9421809c6ef28b8d93539289bef
areascript <areascript22@gmail.com>
2025-07-29 19:25:24 -0500

    Add login page

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:blog_app/init_dependencies.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+//We are on feature/login-page branch
 void main() async {
   await initDependencies();
   runApp(
